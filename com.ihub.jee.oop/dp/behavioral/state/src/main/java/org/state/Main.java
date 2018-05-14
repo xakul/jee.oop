8ad1e3f8e69f2aa2e4c29124c1aa04d1a123fd10
xcdincay <32479569+xcdincay@users.noreply.github.com>
2018-05-14 16:08:55 +0300

    State Pattern

---
+++ com.ihub.jee.oop/dp/behavioral/state/src/main/java/org/state/Main.java
@@ -0,0 +1,26 @@
+package org.state;
+
+public class Main {
+	public static void main(String[] args) {
+		/**
+		 * Burada robotumuza istediğimiz askiyonları vererek hiç bir if ve ya herhangi bir
+		 * condition kullanmadan sürecimizin devam ettiğini göreceğiz.
+		 */
+		/**
+		 * We will see the actions of the robot.We didnt use any if or conditions 
+		 * and our progress is still going perfectly by this pattern.
+		 */
+		Robot robot = new Robot();
+		robot.take();
+		robot.Standby();
+		robot.take();
+		robot.put();
+		
+		robot.take();
+		robot.put();
+		robot.put();
+		
+		robot.Standby();
+		robot.Standby();
+	}
+}
