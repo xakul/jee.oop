0b8a4e0035a282cc6c0d7021415d6ebb5b7975ed
Ali Baran Ozden <alibaranozden@yandex.com>
2018-05-14 17:42:38 +0300

    sf read and chainofresposibility

--- com.ihub.jee.oop/dp/behavioral/chainOfResponsibility/src/main/java/atm/Dollar50Dispenser.java
+++ com.ihub.jee.oop/dp/behavioral/chainOfResponsibility/src/main/java/atm/Dollar50Dispenser.java
@@ -5,7 +5,9 @@ public class Dollar50Dispenser implements DispenseChain {
 
 	public void setNextChain(DispenseChain nextChain) {
 		/*
-		 * The task(chain) that comes after is set as the chain for this function
+		 * 
+		 * function
+		 *
 		 * Bundan sonra gelecek iş(zincir), bu objenin zinciri olarak atanır.
 		 */
 		this.chain = nextChain;
@@ -13,25 +15,27 @@ public class Dollar50Dispenser implements DispenseChain {
 
 	public void dispense(Currency currency) {
 		/*
-		 * If this chain is responsible for the job, do the job. (Hence the if check)
-		 * Eğer bu işten bu zincir sorumluysa yapsın. ("If check", bunu kontrol ediyor)
+		 * If this chain is responsible for the job, do the job. (Hence the if
+		 * check)
+		 * 
+		 * Eğer bu işten bu zincir sorumluysa yapsın. ("If check", bunu kontrol
+		 * ediyor)
 		 */
 		if (currency.getAmount() >= 50) {
 			int number = currency.getAmount() / 50;
 			int remainder = currency.getAmount() % 50;
 			System.out.println("Dispensing " + number + " 50$ note");
-			
+
 			/*
 			 * If the remainder is not zero, transfer it to the next chain.
+			 * 
 			 * Kalan sıfırdan farklı ise, kalanı sonraki zincire aktar.
 			 */
 			if (remainder != 0)
 				this.chain.dispense(new Currency(remainder));
-		}
-		else {
+		} else {
 			/*
-			 * İşi sonraki zincire devir et.
-			 * Transfer the job to the next chain
+			 * İşi sonraki zincire devir et. Transfer the job to the next chain
 			 */
 			this.chain.dispense(currency);
 		}
--- com.ihub.jee.oop/dp/behavioral/chainOfResponsibility/src/main/java/atm/Main.java
+++ com.ihub.jee.oop/dp/behavioral/chainOfResponsibility/src/main/java/atm/Main.java
@@ -1,4 +1,11 @@
 package atm;
+/**
+ * chainOfresponsibility 
+ * This project introduces the implementation of
+ * chainOfresponsibility 
+ * Release version 1.0 at 14.05.2018
+ * @author Mehmet Gurol || @github xmgurol
+ */
 
 import java.util.Scanner;
 
@@ -6,30 +13,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		ATMDispenseChain atmDispenser = new ATMDispenseChain();
-		
+
 		/*
 		 * Read the amount from the user.
+		 * 
 		 * Değeri kullanıcıdan oku.
 		 */
 		System.out.println("Enter amount to dispense");
 		Scanner input = new Scanner(System.in);
 		int amount = input.nextInt();
 		input.close();
-		
+
 		/*
 		 * Amount should be in multiple of 10s.
+		 * 
 		 * Değer, 10 ve katları olmalıdır.
 		 */
 		if (amount % 10 != 0) {
 			System.out.println("Amount should be in multiple of 10s.");
 			return;
 		}
-		
-		/* 
-		 * Start the chain.
+
+		/*
+		 * Start the chain.0
+		 * 
 		 * Zinciri başlat.
 		 */
 		atmDispenser.chain1.dispense(new Currency(amount));
 	}
-	
+
 }
